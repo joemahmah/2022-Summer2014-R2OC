919e4d455ea93177b2d339f3c2974fdef6433edb
joemahmah <mhrcek@imsa.edu>
2014-07-24 22:47:50 -0500

    a

--- 2022-summer2014-r2oc/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ 2022-summer2014-r2oc/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -34,7 +34,7 @@ public class RobotMap {
     public static final double movePercentage = .7;
     public static final double turnPercentage = .3;
     
-    public static final Jaguar turnMotors[] = {new Jaguar(1),new Jaguar(2),new Jaguar(3),new Jaguar(4)};
-    public static final Jaguar driveMotors[] = {new Jaguar(5),new Jaguar(6),new Jaguar(7),new Jaguar(8)};
+    public static final Jaguar turnMotors[] = {new Jaguar(7),new Jaguar(2),new Jaguar(4),new Jaguar(5)};
+    public static final Jaguar driveMotors[] = {new Jaguar(8),new Jaguar(1),new Jaguar(3),new Jaguar(6)};
     public static final Encoder turnEncoders[] = {new Encoder(3, 4, true, CounterBase.EncodingType.k2X),new Encoder(3, 4, true, CounterBase.EncodingType.k2X),new Encoder(3, 4, true, CounterBase.EncodingType.k2X),new Encoder(3, 4, true, CounterBase.EncodingType.k2X)};
 }
